bd3e6c8300937f65b66bcb51cace67966dd84f5a
Isabelle Hu <isabellehu317@gmail.com>
2020-09-05 15:34:53 -0400

    Update remove_ipynb_checkpoints.sh

--- remove_ipynb_checkpoints.sh
+++ remove_ipynb_checkpoints.sh
@@ -1,4 +1,1 @@
-# to remove the file on git without deleting from local
-#  use -> git rm --cached 
-find . -name .ipynb_checkpoints -print0 | xargs -0 git rm -r --ignore-unmatch
-echo .ipynb_checkpoints >> .gitignore
+rm -rf `find -type d -name .ipynb_checkpoints`
